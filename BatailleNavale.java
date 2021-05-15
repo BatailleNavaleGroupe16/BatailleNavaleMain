6368a0bf5647dfe79aeb943986149f020672e507
thomasdelacombe <83460045+thomasdelacombe@users.noreply.github.com>
2021-05-15 19:45:37 +0200

    Update BatailleNavale.java

--- BatailleNavale.java
+++ BatailleNavale.java
@@ -1,5 +1,103 @@
-//Version 8/05/21
+//Version 15/05/21
 import java.util.Scanner;
+
+public class BatailleNavale {
+    
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        
+        //Initialisation : choix du mode jeu : JVJ, JVO
+        
+        byte typeJeu = 0;
+        byte typeMax = 3;
+        
+        do{
+            System.out.println("Recherche des bateaux de l'ordinateur : 1 - Joueur contre Joueur : 2 - Joueur contre Ordinateur : 3");
+            System.out.print("Votre choix : ");
+            typeJeu = sc.nextByte();
+        }while ((typeJeu <=0) || (typeJeu > typeMax));
+        
+        
+        //Création des joueurs
+        
+        Joueur j1;
+        Joueur j2;
+        
+        Joueur[] tabJ = new Joueur[2];
+        
+        /*
+        si 1 => j1(2) j2(3)
+        si 2 => j1(1) j2(1)
+        si 3 => j1(1) j2(4)
+        */
+        
+        if (typeJeu == 1){
+            j1 = new Joueur((byte)2);
+            j2 = new Joueur((byte)3);
+        }else if(typeJeu == 2){
+            j1 = new Joueur((byte)1);
+            j2 = new Joueur((byte)1);
+        }else{
+            j1 = new Joueur((byte)1);
+            j2 = new Joueur((byte)4);
+        }
+        
+        tabJ[0] = j1;
+        tabJ[1] = j2;
+        
+        
+        
+        
+        /*boucle du jeu : Affichage des coups joués, nb bateaux restants à adversaire.
+        choix case à attaquer : check si déjà attaquée ou pas
+        */
+        
+        byte actuel = 1; // pour commencer par joueur 1 dans boucle
+        byte autre = (byte)((actuel + 1)%2);
+        boolean enCours = true;
+        
+        while (enCours){
+            
+            actuel = (byte)((actuel + 1)%2);
+            autre = (byte)((actuel + 1)%2);
+            
+            System.out.println("\n\n\n\n\n"+"Joueur " + (actuel+1));
+            
+            //Affichage bateau
+            tabJ[actuel].afficheCoups(tabJ[autre].nbBateaux);
+            
+            //Affichage nb bateaux restants
+            tabJ[actuel].Attaque(tabJ[autre]);
+            
+            
+            System.out.println("Tour joué");
+            
+            enCours = tabJ[autre].fini();
+            
+            
+        }
+        
+        System.out.println("Le joueur " + (actuel+1) + " a gagné cette partie");
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+
+
+
+
+
+//Version 8/05/21
+/*import java.util.Scanner;
 
 public class BatailleNavale {
     
@@ -41,7 +139,7 @@ public class BatailleNavale {
         choix case à attaquer : check si déjà attaquée ou pas
         */
         
-        byte actuel = 1; // pour commencer par joueur 1 dans boucle
+ /*       byte actuel = 1; // pour commencer par joueur 1 dans boucle
         
         if (typeJeu == 1){
             actuel = 0; //comme ça pas besoin de changer de joueur1 dans la suite
@@ -81,7 +179,7 @@ public class BatailleNavale {
     
     
     
-}
+}*/
 
 
 
